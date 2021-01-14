ebdf37444a9c840da17761b011720561c84ae5fe
zfinnan <63361320+zfinnan@users.noreply.github.com>
2021-01-14 12:46:39 -0500

    add model and cats to database

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index"),
-    path('about/', views.about, name="about"),
+  path('', views.index, name="index"),
+  path('about/', views.about, name="about"),
+  path('cats/', views.cats_index, name="cats_index"),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Cat
 
 # Create your views here.
 def index(request):
@@ -6,3 +7,8 @@ def index(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def cats_index(request):
+    cats = Cat.objects.all()
+    return render(request, 'cats/index.html', {'cats': cats})
+
